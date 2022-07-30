57a015694635e1911449aeab5e56f19897a6c950
InsertSickName <94913361+InsertSickName@users.noreply.github.com>
2022-07-30 13:00:14 +0530

    Update Cache_Server.py

--- Cache_Server.py
+++ Cache_Server.py
@@ -1,72 +1,15 @@
-import socket
-import pickle
-import struct
-import threading
-import time
+import os
 
-frame = None
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-port = 9669
-print("Client Serving Server")
-print("Host IP:", host_ip)
-server_socket.bind((host_ip, port))
-server_socket.listen()
-print("Listening at:", (host_ip, port), "\n\n")
+import redis
+from rq import Worker, Queue, Connection
 
+listen = ['high', 'default', 'low']
 
-def video_recv():
-    global frame
-    receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_name = socket.gethostname()
-    host_ip = socket.gethostbyname(host_name)
-    port = 6969
-    print("Victim Server: ")
-    print("Host IP:", host_ip)
-    receive_socket.bind((host_ip, port))
-    receive_socket.listen(1)
-    print("Listening at:", (host_ip, port), "\n\n")
-    victim_socket, addr = receive_socket.accept()
-    print(f"Victim Connected at {addr}")
-    data = b""
-    payload_size = struct.calcsize("Q")
-    while True:
-        while len(data) < payload_size:
-            packet = victim_socket.recv(4*1024)
-            if not packet:
-                break
-            data += packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q", packed_msg_size)[0]
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
-        while len(data) < msg_size:
-            data += victim_socket.recv(1024*1024)
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
-        frame = pickle.loads(frame_data)
+conn = redis.from_url(redis_url)
 
-
-threading.Thread(target=video_recv).start()
-
-
-def serve_client(addr, client_socket):
-    global frame
-    try:
-        print(f"Client {addr} Connected!")
-        if client_socket:
-            while True:
-                a = pickle.dumps(frame)
-                message = struct.pack("Q", len(a)) + a
-                client_socket.sendall(message)
-    except Exception as e:
-        print(f"Client {addr} Disconnected!")
-
-
-while True:
-    client_socket, addr = server_socket.accept()
-    print(addr)
-    threading.Thread(target=serve_client, args=(addr, client_socket)).start()
-    time.sleep(0.1)
-    print("Total Clients:", threading.active_count()-2)
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
